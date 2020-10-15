2eb59e2e9cd160927d08d8b1bb20ea8d810a3aaf
Nikita Teremovskii <ni-2@mail.ru>
2020-10-15 12:35:35 +0300

    Renamed src/buildTree.js to src/treeBuilder.js and some constants in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import fs from 'fs';
 import path from 'path';
-import getParser from './parsers.js';
-import formatWith from './formatters/index.js';
-import buildTree from './buildTree.js';
+import parse from './parsers.js';
+import format from './formatters/index.js';
+import buildTree from './treeBuilder.js';
 
 const getData = (filepath) => {
   const absoluteFilename = path.resolve(process.cwd(), filepath);
   const data = fs.readFileSync(absoluteFilename, 'utf-8');
-  const fileType = path.extname(filepath).slice(1);
-  const parse = getParser(fileType);
-  return parse(data);
+  const type = path.extname(filepath).slice(1);
+  return parse(type)(data);
 };
 
-export default (filepath1, filepath2, formatter = 'stylish') => {
+export default (filepath1, filepath2, outputFormat = 'stylish') => {
   const data1 = getData(filepath1);
   const data2 = getData(filepath2);
   const diff = buildTree(data1, data2);
-  return formatWith(formatter)(diff);
+  return format(outputFormat)(diff);
 };
